721d1a97b547c9ede52c7a11ff045f49d97a5fbb
Sameer-0077 <165882062+Sameer-0077@users.noreply.github.com>
2024-07-13 16:15:02 +0000

    DayOne

---
+++ DayOne/dayOne.js
@@ -0,0 +1,47 @@
+
+// Activity 1 : Variable Declaration
+
+// Task 1
+var num = 9898;
+console.log(num);
+
+// Task 2
+let str = "This is a String";
+console.log(str);
+
+// Activity 2 : Constant Declaration
+const condition = true;
+console.log(condition);
+
+// Activity 3 : Data Types
+
+let num2 = 2024;
+console.log(`Data type of num2 is ${typeof(num2)}`);
+
+let str2 = "This is another String";
+console.log(`Data type of str2 is ${typeof(str2)}`);
+
+const arr = [1,2,3,4,5,6];
+console.log(`Data type of arr is ${typeof(arr)}`);
+
+const obj = {
+    name : "Sameer",
+    location : "India",
+    year : 2024
+}
+console.log(`Data type of obj is ${typeof(obj)}`);
+
+// Activity 4 : Reassigning Variable
+
+let score = 200;
+console.log(score);
+
+score = 210;
+console.log(score);
+
+// Activity 5 : Understanding const
+
+const valueOfPi = 3.14;
+console.log(valueOfPi);
+
+valueOfPi = 4.13; // This line give error!
